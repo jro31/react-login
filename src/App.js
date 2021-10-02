d265e5ed802dc2295b0adda2064d2c8f96149d5a
Jethro Williams <jethro.williams@thirdspacelearning.com>
2021-10-02 16:15:19 +0100

    Use useEffect with localStorage to store and check whether a user is logged-in or not, and render the app accordingly

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,28 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    // This funtion will always run when the component is first rendered, AFTER the rest of the component is evaluated
+    // After this, it will only run when the dependencies (the second argument) change
+    // As there are no dependencies here (an empty array), it will not run again, even if the component is re-evaluated
+    // As the state 'isLoggedIn' is updated within this function, is causes the component function to run again BUT this code (within useEffect) will not be re-run because its dependencies were not updated
+
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    };
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
