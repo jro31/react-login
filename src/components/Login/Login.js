24135ccdc04c49774243e03f563bd86b5e2544fd
Jethro Williams <jethro.williams@thirdspacelearning.com>
2021-10-02 18:29:56 +0100

    Add a useEffect cleanup function with a timer, to run a function only once the user has stopped typing for half a second

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -13,9 +13,19 @@ const Login = (props) => {
 
   useEffect(() => {
     // Will run whenever the dependencies ([enteredEmail, enteredPassword]) change
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    // This return (which must be a function) is called a "clean-up function" - it runs at the beginning of this 'useEffect' function, except when 'useEffect' is called for the first time
+    // Used like this, it will clear the timer (the identifier variable) each time that 'useEffect' is called
+    // This means that the 'setFormIsValid' function will not run unless it has been 500ms since this 'useEffect' function was last called
+    // This would be useful if, for example, instead of 'setFormIsValid', we had a more intensive action here (such as an API call) and only wanted to run it once the user had stopped typing
+    return () => {
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
