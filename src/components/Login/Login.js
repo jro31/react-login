7f8f4649b89848a42ab8ba532bd655b6ce01d499
Jethro Williams <jethro.williams@thirdspacelearning.com>
2021-10-03 15:44:14 +0100

    Use useReducer to store the email value, and email is valid properties as one state

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,43 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+// Should return the new state
+// The 'state' argument is implied, and is guaranteed to be the last state snapshot
+// The action argument, is what you pass-in as an argument when calling this function
+const emailReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.includes('@') };
+  };
+  if (action.type === 'INPUT_BLUR') {
+    return { value: state.value, isValid: state.value.includes('@') };
+  };
+  return { value: '', isValid: false };
+};
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  useEffect(() => {
-    // Will run whenever the dependencies ([enteredEmail, enteredPassword]) change
-    const identifier = setTimeout(() => {
-      setFormIsValid(
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6
-      );
-    }, 500);
-
-    // This return (which must be a function) is called a "clean-up function" - it runs at the beginning of this 'useEffect' function, except when 'useEffect' is called for the first time
-    // Used like this, it will clear the timer (the identifier variable) each time that 'useEffect' is called
-    // This means that the 'setFormIsValid' function will not run unless it has been 500ms since this 'useEffect' function was last called
-    // This would be useful if, for example, instead of 'setFormIsValid', we had a more intensive action here (such as an API call) and only wanted to run it once the user had stopped typing
-    return () => {
-      clearTimeout(identifier);
-    };
-  }, [enteredEmail, enteredPassword]);
+  // useReducer arguments: (reducer function, initial state, initial function)
+  // (the initial function parameter is optional, and not included here)
+  const [emailState, dispatchEmail] = useReducer(emailReducer, { value: '', isValid: null },);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({ type: 'USER_INPUT', val: event.target.value });
+
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length > 6
+    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+
+    setFormIsValid(
+      emailState.isValid && event.target.value.trim().length > 6
+    );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    dispatchEmail({ type: 'INPUT_BLUR' });
   };
 
   const validatePasswordHandler = () => {
@@ -46,7 +52,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value , enteredPassword);
   };
 
   return (
@@ -54,14 +60,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
