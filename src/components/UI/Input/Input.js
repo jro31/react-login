30e04395275f4fbf84840127aac0921d937466ba
Jethro Williams <jethro.williams@thirdspacelearning.com>
2021-10-09 19:25:00 +0100

    Use the 'focus()' function (with refs) to focus on a particular input
    Not particularly useful in the way it's used here, but an example of using 'focus()' at least

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 import classes from './Input.module.css';
 
 const Input = (props) => {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    // As useEffect is only called after everything else loads, the 'inputRef' will be the last input to load, which is the password
+    // This function will then 'focus' on the password input (make it the focus of the page, where the cursor is)
+    // (as we don't include any dependencies, this will only happen on page load)
+    inputRef.current.focus();
+  }, []);
+
   return (
     <div
       className={`${classes.control} ${
@@ -11,6 +20,7 @@ const Input = (props) => {
     >
       <label htmlFor={props.id}>{props.label}</label>
       <input
+        ref={inputRef}
         type={props.type}
         id={props.id}
         value={props.value}
